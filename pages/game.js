05d8ff6951e74074cc2b72cc7ad54de43f3aa578
Colin McNeil <colinmcneil@gmail.com>
2018-02-20 12:24:12 -0500

    Made welcome page

---
+++ pages/game.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import game_component from '../components/game';
+
+export default class SimplePage {
+	getElements() {
+		return <game_component/>;
+	}
+
+	getMetaTags() {
+		return [
+			{charset: 'utf8'},
+			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{name: 'description', content: 'hello world, powered by React Server'},
+			{name: 'generator', content: 'React Server'},
+		];
+	}
+}
