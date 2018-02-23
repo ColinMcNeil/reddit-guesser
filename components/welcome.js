d82311a73da4e6416895c70eaf660479abebab01
Colin McNeil <colinmcneil@gmail.com>
2018-02-22 22:39:17 -0500

    Started captcha work

--- components/welcome.js
+++ components/welcome.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import {logging} from 'react-server';
 const logger = logging.getLogger(__LOGGER__);
+const site_key = '1Qhl0m0kKwWrfowA3n0i2dt3G7c3yEZq'
+const captcha = (
+	<div className="coinhive-captcha" data-hashes="512" data-key='' >
+		<em>
+			Loading Captcha...<br/>
+				If it doesn't load, please disable Adblock!
+		</em>
+	</div>
+)
 
 export default class Welcome extends React.Component {
 	constructor(props) {
@@ -8,17 +17,37 @@ export default class Welcome extends React.Component {
 		this.state = {buttonText: 'Play'};
 		this.play = () => {
 			this.setState({ buttonText: 'Loading' })
-			window.location = '/game';
+			this.clickedPlay = true;
 		}
 	}
+	componentDidMount() {
+		const script = document.createElement("script");
+
+		script.src = "https://authedmine.com/lib/captcha.min.js";
+		script.async = true;
+
+		document.body.appendChild(script);
+	}
 	render() {
+		let button = <div id="enter" onClick={this.play}>{this.state.buttonText}</div>
+		if (this.clickedPlay) {
+			button = captcha;
+		}
 		return (
 			<div id="background">
+
 				<div id="title_container">
 					<h1>Reddit Guesser</h1>
-					<p>Guess which subreddit each image is from!</p>
-					<div id="enter" onClick={this.play}>{this.state.buttonText}</div>
+					<p>
+						Guess which subreddit each image, text post, or article is from!<br />
+						Currently indexing <strong>60,871</strong> random subreddits!<br />
+						<strong>May contain NSFW Content</strong><br />
+						Over 18 Subreddits, and Subreddits with under 100 subscribers NOT included.
+					</p>
+					{button}
+					
 				</div>
+				
 			</div>	
 		);
 	}
